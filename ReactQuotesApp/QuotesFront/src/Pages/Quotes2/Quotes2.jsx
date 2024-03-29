e0b1f5eb7386f820f5207a8b142487e8a18c2ca5
Kastriot <kaloti.avdiaj@gmail.com>
2024-03-29 18:40:55 +0100

    IMported 3 new fonts, And styled the new Quotes page.

--- ReactQuotesApp/QuotesFront/src/Pages/Quotes2/Quotes2.jsx
+++ ReactQuotesApp/QuotesFront/src/Pages/Quotes2/Quotes2.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { QuotesContext } from "../../Components/Quotes/QuotesProvider";
+import Quote2 from "../../Components/Quotes/Quote2";
+import "./Quotes2.css";
+import Divider from "@mui/material/Divider";
 
 export const Quotes2 = () => {
-  return <div>Quotes2</div>;
+  const { quotes, addQuote, setQuotes } = useContext(QuotesContext);
+
+  return (
+    <>
+      <ul className="quotes2Ul">
+        {quotes.map((quote, index) => (
+          <li
+            key={quote.id}
+            className={index % 2 === 0 ? "quotes2Li even" : "quotes2Li odd"}
+          >
+            <Quote2
+              description={quote.description}
+              authorName={quote.authorName}
+            ></Quote2>
+            <Divider sx={{ backgroundColor: "gray" }} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
