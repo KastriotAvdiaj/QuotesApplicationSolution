1a296f7a0bc95d6418b9325c2d567e5fb47a1565
Kastriot <kaloti.avdiaj@gmail.com>
2024-04-01 19:03:00 +0200

    Showing only a few quotes at a time and introduced 2 buttons to show more or less quotes. Added a fade-in effect to the quotes each time they're scolled in and out of view.

--- ReactQuotesApp/QuotesFront/src/Components/Quotes/Quote2.jsx
+++ ReactQuotesApp/QuotesFront/src/Components/Quotes/Quote2.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./Quote2.css";
-const Quote2 = ({ description, authorName, id }) => {
+const Quote2 = ({ description, authorName, index }) => {
   return (
     <div className="quote2Container">
+      <p className="index">{index}.</p>
       <p className="quote2-description">"{description}"</p>
       <p className="quote2-author">-{authorName}</p>
     </div>
--- ReactQuotesApp/QuotesFront/src/Pages/Quotes2/Quotes2.jsx
+++ ReactQuotesApp/QuotesFront/src/Pages/Quotes2/Quotes2.jsx
@@ -1,28 +1,100 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { QuotesContext } from "../../Components/Quotes/QuotesProvider";
 import Quote2 from "../../Components/Quotes/Quote2";
 import "./Quotes2.css";
 import Divider from "@mui/material/Divider";
+import { TfiReload } from "react-icons/tfi";
+import { TfiUpload } from "react-icons/tfi";
 
 export const Quotes2 = () => {
   const { quotes, addQuote, setQuotes } = useContext(QuotesContext);
+  const [displayedQuotes, setDisplayedQuotes] = useState([]);
+  const [numberOfQuotesToShow, setNumberOfQuotesToShow] = useState(7);
+  const [incrementBy, setIncrementBy] = useState(5);
+  const [buttonClicked, setButtonClicked] = useState(0);
+
+  useEffect(() => {
+    setDisplayedQuotes(quotes.slice(0, numberOfQuotesToShow));
+  }, [quotes, numberOfQuotesToShow]);
+
+  const loadMoreQuotes = () => {
+    const newNumberOfQuotesToShow = numberOfQuotesToShow + incrementBy;
+    setDisplayedQuotes(quotes.slice(0, newNumberOfQuotesToShow));
+    setNumberOfQuotesToShow(newNumberOfQuotesToShow);
+    setButtonClicked(buttonClicked + 1);
+  };
+
+  const showLessQuotes = () => {
+    const newNumberOfQuotesToShow = numberOfQuotesToShow - incrementBy;
+    setDisplayedQuotes(quotes.slice(0, newNumberOfQuotesToShow));
+    setNumberOfQuotesToShow(newNumberOfQuotesToShow);
+    setButtonClicked(buttonClicked - 1);
+  };
 
   return (
     <>
-      <ul className="quotes2Ul">
-        {quotes.map((quote, index) => (
-          <li
-            key={quote.id}
-            className={index % 2 === 0 ? "quotes2Li even" : "quotes2Li odd"}
-          >
-            <Quote2
-              description={quote.description}
-              authorName={quote.authorName}
-            ></Quote2>
-            <Divider sx={{ backgroundColor: "gray" }} />
-          </li>
-        ))}
-      </ul>
+      <div className="mainQuotes2Div">
+        <ul className="quotes2Ul">
+          {displayedQuotes.map((quote, index) => (
+            <FadeInListItem key={quote.id} index={index} quote={quote} />
+          ))}
+        </ul>
+        {numberOfQuotesToShow < quotes.length && (
+          <div className="quotes2ButtonDiv">
+            <button className="loadMoreButton" onClick={loadMoreQuotes}>
+              Load More
+              <TfiReload />
+            </button>
+            {buttonClicked > 0 && (
+              <button className="loadMoreButton" onClick={showLessQuotes}>
+                Show Less
+                <TfiUpload />
+              </button>
+            )}
+          </div>
+        )}
+      </div>
     </>
+  );
+};
+
+const FadeInListItem = ({ index, quote }) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          ref.current.classList.add("fade-in");
+        } else {
+          ref.current.classList.remove("fade-in");
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return (
+    <li
+      ref={ref}
+      className={index % 2 === 0 ? "quotes2Li even" : "quotes2Li odd"}
+    >
+      <Quote2
+        index={index}
+        description={quote.description}
+        authorName={quote.authorName}
+      ></Quote2>
+      <Divider sx={{ backgroundColor: "gray", height: "1px" }} />
+    </li>
   );
 };
